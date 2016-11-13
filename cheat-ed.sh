0ed6fbfcebbb271b126d1048453448b4c27bbd37
Jakukyo Friel <weakish@gmail.com>
2016-11-13 21:30:25 +0800

    cheat-ed: fallback to vim/vi if editor is not available.

--- cheat-ed.sh
+++ cheat-ed.sh
@@ -6,6 +6,15 @@ set -u
 ### edit a cheatsheet
 
 cd "$CHEAT_REPO"
-editor $1.md
-git add $1.md
+if (which editor &> /dev/null); then
+    editor "$1.md"
+elif (which vim &> /dev/null); then
+    vim "$1.md"
+elif (which vi &> /dev/null); then
+    vi "$1.md"
+else
+    echo "Cannot find vi/vim."
+    exit 69 # EX_UNAVAILABLE
+fi    
+git add "$1.md"
 git commit -m "Create or update $1"
